976639e911e6d738db538714d216f91676ae9ab8
Batuhan MERMER <batuhanmrmr@gmail.com>
2023-04-26 17:49:51 +0300

    Update trial_1.py

--- trial_1.py
+++ trial_1.py
@@ -2,7 +2,7 @@ from rovpy import rovpy
 import time
 
 
-rovpy.connectrov("STABILIZE","/dev/ttyACM0")  #connectrov(mode,port or udp url) düzenleyin bunu
+rovpy.connectrov("MANUAL","/dev/ttyACM0")  #connectrov(mode,port or udp url) düzenleyin bunu
 
 rovpy.arm()
 
